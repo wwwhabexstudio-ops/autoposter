7f371fa8105f3d356301057961a54456f8b86b0b
wwwhabexstudio-ops <www.habexstudio@gmail.com>
2026-08-26 08:18:38 +0100

    Add Meta platform adapter placeholder

---
+++ platforms/meta.py
@@ -0,0 +1,11 @@
+"""Placeholder for official Meta publishing integration.
+
+Instagram/Facebook publishing will use Meta's official APIs. Secrets are
+provided through deployment environment variables, never committed here.
+"""
+
+META_API_VERSION = "v23.0"
+
+
+def configured() -> bool:
+    return False
